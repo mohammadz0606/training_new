4862eab5af73c50c4e22d6d0bf7e0182b9d3ae4c
Mohammad Zeino <105930030+mohammadz0606@users.noreply.github.com>
2023-08-06 11:31:31 +0300

    task 4

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,118 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const MyGridView(),
     );
   }
+}
+
+class MyGridView extends StatelessWidget {
+  const MyGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'GridView',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+        ),
+        itemCount: gridItems.length,
+        itemBuilder: (context, index) {
+          ImageWithTitle item = gridItems[index];
+          return GridItem(imagePath: item.imagePath, title: item.title);
+        },
+      ),
+    );
+  }
+}
+class GridItem extends StatelessWidget {
+  final String imagePath;
+  final String title;
+
+  const GridItem({
+    super.key,
+    required this.imagePath,
+    required this.title,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Stack(
+        children: [
+          Image.network(
+            imagePath,
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            padding: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.transparent, Colors.black.withOpacity(0.6)],
+              ),
+            ),
+            child: Text(
+              title,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  Shadow(
+                    color: Colors.black.withOpacity(0.6),
+                    offset: const Offset(1, 1),
+                    blurRadius: 3,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+List<ImageWithTitle> gridItems = [
+  ImageWithTitle(
+      imagePath:
+          "https://images.unsplash.com/photo-1690544732518-02d5110ce233?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+      title: "Image 1"),
+  ImageWithTitle(imagePath: "https://images.unsplash.com/photo-1599534456053-486d04fce9de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80", title: "Image 2"),
+  ImageWithTitle(imagePath: "https://images.unsplash.com/photo-1682685797741-f0213d24418c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80", title: "Image 3"),
+  ImageWithTitle(imagePath: "https://images.unsplash.com/photo-1682686578842-00ba49b0a71a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=775&q=80", title: "Image 4"),
+  ImageWithTitle(imagePath: "https://images.unsplash.com/photo-1661956602153-23384936a1d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80", title: "Image 5"),
+  ImageWithTitle(imagePath: "https://images.unsplash.com/photo-1690718701767-4af5cdab195e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80", title: "Image 6"),
+  ImageWithTitle(imagePath: "https://images.unsplash.com/photo-1690983322475-29a61c585204?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=404&q=80", title: "Image 7"),
+  ImageWithTitle(imagePath: "https://images.unsplash.com/photo-1691080994369-0fb7784e37be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=725&q=80", title: "Image 8"),
+  ImageWithTitle(imagePath: "https://plus.unsplash.com/premium_photo-1674676471622-feaf2130fc73?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80", title: "Image 9"),
+];
+
+class ImageWithTitle {
+  final String imagePath;
+  final String title;
+
+  ImageWithTitle({
+    required this.imagePath,
+    required this.title,
+  });
 }
