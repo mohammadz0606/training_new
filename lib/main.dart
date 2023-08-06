fe1cbc5edfa9433057045984bae14d40657694b0
Mohammad Zeino <105930030+mohammadz0606@users.noreply.github.com>
2023-08-06 11:56:25 +0300

    task 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SwitchApp());
 }
 
+/*
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,6 +15,53 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+      ),
+      home: SwitchApp(),
+    );
+  }
+}
+*/
+
+
+class SwitchApp extends StatefulWidget {
+  const SwitchApp({super.key});
+
+  @override
+  State<SwitchApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<SwitchApp> {
+  bool _isDarkMode = false;
+
+  void _toggleDarkMode(bool value) {
+    setState(() {
+      _isDarkMode = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text(_isDarkMode ? "Dark Theme" : "Light Theme")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                _isDarkMode ? 'Dark Mode' : 'Light Mode',
+                style: const TextStyle(fontSize: 24),
+              ),
+              const SizedBox(height: 20),
+              Switch.adaptive(
+                value: _isDarkMode,
+                onChanged: _toggleDarkMode,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
