005be3b5cfd55f999edc63f097a25c009c5399e5
Mohammad Zeino <105930030+mohammadz0606@users.noreply.github.com>
2023-08-06 11:45:35 +0300

    task 4

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,81 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const ButtonsInFlutter(),
+    );
+  }
+}
+
+class ButtonsInFlutter extends StatelessWidget {
+  const ButtonsInFlutter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Button Examples',style: TextStyle(
+          color: Colors.white
+        ),),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Elevated Button'),
+            ),
+            const SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text('Outlined Button'),
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Text Button'),
+            ),
+            const SizedBox(height: 20),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.thumb_up),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.mail),
+              label: const Text('Elevated Button with Icon'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.orange),
+              ),
+              child: const Text('Custom Elevated Button'),
+            ),
+            const SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                side: MaterialStateProperty.all(
+                    const BorderSide(color: Colors.red)),
+              ),
+              child: const Text('Custom Outlined Button'),
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.green),
+              ),
+              child: const Text('Custom Text Button'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
