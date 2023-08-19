597090bc17d03fb4d255d64ce39aaa7d201011cb
Mohammad Zeino <105930030+mohammadz0606@users.noreply.github.com>
2023-08-19 16:44:23 +0300

    task 9

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +18,62 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? _image;
+
+  final _picker = ImagePicker();
+  Future<void> _openImagePicker() async {
+    final XFile? pickedImage =
+    await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image Piker'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(35),
+          child: Column(children: [
+            Center(
+              child: ElevatedButton(
+                onPressed: ()async{
+                  await _openImagePicker();
+                },
+                child: const Text('Select An Image'),
+              ),
+            ),
+            const SizedBox(height: 35),
+            Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: 300,
+              color: Colors.grey[300],
+              child: _image != null
+                  ? Image.file(_image!, fit: BoxFit.cover)
+                  : const Text('Please select an image'),
+            )
+          ]),
+        ),
+      ),);
   }
 }
