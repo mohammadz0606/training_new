7636e47fc8c4b75d39d2d21fcbec4a856acf34a4
Mohammad Zeino <105930030+mohammadz0606@users.noreply.github.com>
2024-01-08 12:39:00 +0300

    edit kotlin

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:sts_one_pay/models/sts_one_pay.dart';
+import 'package:sts_one_pay/sts_one_pay_method_channel.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +15,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData.light().copyWith(
         useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        title: const Text(
+          'StsOnePay',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            MethodChannelStsOnePay methodChannelStsOnePay =
+                MethodChannelStsOnePay();
+            await methodChannelStsOnePay.openPaymentPage(
+              StsOnePay(
+                amount: '5000',
+                currency: '682',
+              ),
+              onResultResponse: (result) {
+                log('Token');
+                log(result.token ?? '');
+              },
+              onDeleteCardResponse: (onDeleteCard) {},
+            );
+          },
+          child: const Text('Click'),
+        ),
       ),
     );
   }
